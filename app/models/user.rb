ae5d1beaae963427fd1c7db93e7e9b46eccefb97
Justin_Webb <Webb.n.justin@gmail.com>
2020-11-12 08:45:37 -0600

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :posts, dependent: :destroy
     has_many :comments, dependent: :destroy 
+    
+    validates :username, :email, presence: true , uniqueness: { case_sensitive: false }, length: {maximum: 50}
+  
 end
